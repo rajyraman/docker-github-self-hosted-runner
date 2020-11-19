2d048ba56f30823d9c86e3d2a08367e341d1b157
Natraj Yegnaraman <natraj@dreamingincrm.com>
2020-11-20 09:50:01 +1100

    remove volume mapping as you can view filesystem in latest version of docker extension

--- runner-setup.ps1
+++ runner-setup.ps1
@@ -3,22 +3,6 @@ param (
 [string]$gitHubPersonalAccessToken = $( Read-Host -asSecureString "Enter GitHub Personal Access Token" )
 )
 
-$runnerFolder = "actions-runner"
-
-$version = (Invoke-WebRequest -Uri "https://api.github.com/repos/actions/runner/releases/latest" |
-ConvertFrom-Json |
-Select tag_name).tag_name.SubString(1);
-
-if((Test-Path $runnerFolder) -eq $false)
-{
-    mkdir $runnerFolder;
-}
-
-if((Test-Path "actions-runner-win-x64-$version.zip") -eq $false) {
-    Invoke-WebRequest -Uri "https://github.com/actions/runner/releases/download/v$version/actions-runner-win-x64-$version.zip" -OutFile "actions-runner-win-x64-$version.zip";
-    Expand-Archive -Path "actions-runner-win-x64-$version.zip" -DestinationPath $runnerFolder -Force
-}
-
 $runnerToken = (Invoke-WebRequest -Headers @{ "Accept" = "application/vnd.github.everest-preview+json";"Authorization" = "Token $gitHubPersonalAccessToken" } -Method POST -Uri "https://api.github.com/repos/$repo/actions/runners/registration-token" |
 ConvertFrom-Json |
 Select token).token
--- runner.ps1
+++ runner.ps1
@@ -1,5 +1,13 @@
+$version = (Invoke-WebRequest -Uri "https://api.github.com/repos/actions/runner/releases/latest" -UseBasicParsing |
+ConvertFrom-Json |
+Select tag_name).tag_name.SubString(1);
+
+if((Test-Path "actions-runner-win-x64-$version.zip") -eq $false) {
+    Invoke-WebRequest -UseBasicParsing -Uri "https://github.com/actions/runner/releases/download/v$version/actions-runner-win-x64-$version.zip" -OutFile "actions-runner-win-x64-$version.zip";
+    Expand-Archive -Path "actions-runner-win-x64-$version.zip"  -DestinationPath actions-runner -Force
+}
 if((Test-Path ".credentials") -eq $false)
 {
-    .\config.cmd --unattended --url $env:REPO_URL --token $env:RUNNER_TOKEN --name github-actions-runner-docker-$env:COMPUTERNAME;
+    .\actions-runner\config.cmd --unattended --url $env:REPO_URL --token $env:RUNNER_TOKEN --name github-actions-runner-docker-$env:COMPUTERNAME;
 }
-.\run.cmd;
+.\actions-runner\run.cmd;
